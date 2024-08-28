942a8ea77d50355c570d241050261e2797b569ee
jcribeiro98 <jose@cribeiro.de>
2024-08-28 10:59:31 +0200

    Added the outlier detection necessary modules for VGAN and VMMD

---
+++ src/modules/od_module.py
@@ -0,0 +1,24 @@
+from sel_suod.models.base import sel_SUOD
+import numpy as np
+from src.vgan import VGAN
+from src.vmmd import VMMD
+
+
+class VGAN(VGAN):
+
+    def approx_subspace_dist(self, subspace_count=500):
+        u = self.generate_subspaces(subspace_count)
+        unique_subspaces, proba = np.unique(
+            np.array(u.to('cpu')), axis=0, return_counts=True)
+        self.subspaces = unique_subspaces
+        self.proba = proba
+
+
+class VMMD(VMMD):
+
+    def approx_subspace_dist(self, subspace_count=500):
+        u = self.generate_subspaces(subspace_count)
+        unique_subspaces, proba = np.unique(
+            np.array(u.to('cpu')), axis=0, return_counts=True)
+        self.subspaces = unique_subspaces
+        self.proba = proba
