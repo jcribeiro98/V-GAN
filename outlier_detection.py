c697e1031189ff5f2630bda82c35d647f25bfdf1
jcribeiro98 <jose@cribeiro.de>
2024-09-02 17:36:41 +0200

    Added new functions for executing VMMD, VGAN. Also added the possibility to aggregate differently. Improved modularity

--- outlier_detection.py
+++ outlier_detection.py
@@ -1,4 +1,4 @@
-from src.modules.od_module import VGAN
+from src.modules.od_module import VGAN, VMMD
 import numpy as np
 from pyod.models.ocsvm import OCSVM
 from pyod.models.ecod import ECOD
@@ -18,15 +18,18 @@ from data.get_datasets import load_data
 import json
 import os
 import logging
+from src.modules.tools import aggregator_funct
 logger = logging.getLogger(__name__)
 
 
-def launch_outlier_detection_experiments(dataset_name: str, base_estimators: list, epochs=10, temperature=10) -> dict:
+def launch_outlier_detection_experiments(dataset_name: str, base_estimators: list, epochs: int = 10,
+                                         temperature: float = 10, seed: int = 777, gen_model_to_use: str = "VGAN") -> dict:
     """Launch the outlier detection experiments for a given dataset
 
     Args:
         dataset_name (str): Name of the dataset to load
-        base_estimators (list): List including all base estimators to build the ensemble. If the length is = 1, then an homogeneus ensemble will be fitted.
+        base_estimators (list): List including all base estimators to build the ensemble. If the length is = 1, 
+        then an homogeneus ensemble will be fitted.
         directory (Path): Path to the directory one wishes to load to
     Returns:
         tuple: Returns the AUC, PRAUC and F1 of the ensemble obtained by VGAN subspaces
@@ -35,32 +38,41 @@ def launch_outlier_detection_experiments(dataset_name: str, base_estimators: lis
         "No instance of a pretrained generation model found. Proceeding to train a new Generator.")
     X_train, X_test, y_test = load_data(dataset_name)
 
-    vgan = VGAN(epochs=epochs, temperature=temperature, batch_size=500,
-                path_to_directory=Path() / "experiments" /
-                f"VGAN_{dataset_name}",
-                iternum_d=1, iternum_g=5, lr_G=0.01, lr_D=0.01)
+    if gen_model_to_use == "VGAN":
+        vgan = VGAN(epochs=epochs, temperature=temperature, batch_size=500,
+                    path_to_directory=Path() / "experiments" / "VGAN"
+                    f"VGAN_{dataset_name}",
+                    iternum_d=1, iternum_g=5, lr_G=0.01, lr_D=0.01)
+    elif gen_model_to_use == "VMMD":
+        vgan = VMMD(epochs=epochs,  batch_size=500,
+                    path_to_directory=Path() / "experiments" / "VMMD" /
+                    f"VMMD_{dataset_name}", lr=0.01)
+    else:
+        raise ValueError(f"{gen_model_to_use} is not a generator in the list")
+
     vgan.fit(X_train)
-    vgan.approx_subspace_dist()
+    vgan.seed = seed
+    vgan.approx_subspace_dist(add_leftover_features=True)
     ensemble_model = sel_SUOD(base_estimators=base_estimators, subspaces=vgan.subspaces,
-                              n_jobs=5, bps_flag=False, approx_flag_global=False)
+                              n_jobs=-1, bps_flag=False, approx_flag_global=False)
     ensemble_model.fit(X_train)
     decision_function_scores_ens = ensemble_model.decision_function(
         X_test)
-    decision_function_scores_ens = np.average(
-        decision_function_scores_ens, axis=1, weights=vgan.proba)
-
+    decision_function_scores_ens = aggregator_funct(
+        decision_function_scores_ens, weights=vgan.proba, type="exact")
     return {"Dataset": dataset_name,
             "AUC": auc(y_test, decision_function_scores_ens),
             "PRAUC": average_precision_score(y_test, decision_function_scores_ens),
             "F1": f1_score(y_test, (decision_function_scores_ens > np.quantile(decision_function_scores_ens, .80)) * 1)}
 
 
-def pretrained_launch_outlier_detection_experiments(dataset_name: str, base_estimators: list) -> tuple:
+def pretrained_launch_outlier_detection_experiments(dataset_name: str, base_estimators: list, seed: int = 777, gen_model_to_use: str = "VGAN") -> tuple:
     """Launch the outlier detection experiments for a given dataset
 
     Args:
         dataset_name (str): Name of the dataset to load
-        base_estimators (list): List including all base estimators to build the ensemble. If the length is = 1, then an homogeneus ensemble will be fitted.
+        base_estimators (list): List including all base estimators to build the ensemble. If the length is = 1, 
+        then an homogeneus ensemble will be fitted.
         directory (Path): Path to the directory one wishes to load to
     Returns:
         np.array: Returns the AUC, PRAUC and F1
@@ -69,39 +81,64 @@ def pretrained_launch_outlier_detection_experiments(dataset_name: str, base_esti
         f"Pretrained generator found!")
     X_train, X_test, y_test = load_data(dataset_name)
 
-    # TO DO: Add a function to launch outlier detection with a pretrained instance of VGAN/VMMD (to save training time) using the load_models option.
-    vgan = VGAN()
-    vgan.load_models(Path() / "experiments" /
-                     f"VGAN_{dataset_name}" / "models" / "generator_0.pt", ndims=X_train.shape[1])
-    vgan.approx_subspace_dist()
+    if gen_model_to_use == "VGAN":
+        logger.debug("Loading VGAN")
+        vgan = VGAN()
+        vgan.load_models(Path() / "experiments" / "VGAN" /
+                         f"VGAN_{dataset_name}" / "models" / "generator_0.pt", ndims=X_train.shape[1])
+    elif gen_model_to_use == "VMMD":
+        logger.debug("Loading VMMD")
+        vgan = VMMD()
+        vgan.load_models(Path() / "experiments" / "VMMD" /
+                         f"VMMD_{dataset_name}" / "models" / "generator_0.pt", ndims=X_train.shape[1])
+    vgan.seed = seed
+    vgan.approx_subspace_dist(add_leftover_features=True)
     ensemble_model = sel_SUOD(base_estimators=base_estimators, subspaces=vgan.subspaces,
-                              n_jobs=5, bps_flag=False, approx_flag_global=False)
+                              n_jobs=-1, bps_flag=False, approx_flag_global=False)
     ensemble_model.fit(X_train)
     decision_function_scores_ens = ensemble_model.decision_function(
         X_test)
-    decision_function_scores_ens = np.average(
-        decision_function_scores_ens, axis=1, weights=vgan.proba)
+    decision_function_scores_ens = aggregator_funct(
+        decision_function_scores_ens, weights=vgan.proba, type="avg")
 
+    print(vgan.subspaces.shape[0])
     return {"Dataset": dataset_name,
             "AUC": auc(y_test, decision_function_scores_ens),
             "PRAUC": average_precision_score(y_test, decision_function_scores_ens),
             "F1": f1_score(y_test, (decision_function_scores_ens > np.quantile(decision_function_scores_ens, .80)) * 1)}
 
 
-# TO DO: Add a function to launch outlier detection with a pretrained instance of VGAN/VMMD (to save training time)
+def check_if_myopicity_was_uphold(dataset_name: str, gen_model_to_use="VGAN") -> tuple:
+    """Given the dataset name, the function will return the p-value of the GOF test using the MMD with the recommended 
+    bandwidth in [Look for the paper I don't remember the surname of the authors rn].
+
+    Args:
+        dataset_name (str): Name of the dataset as included in the json file 'datasets_file_name.json'
+
+    Returns:
+        float: p-value for the two-sampe non-parametric GoF test using the MMD with recommended bandwidth (by L2 distances)
+    """
+    X_train, _, _ = load_data(dataset_name)
+
+    if gen_model_to_use == "VGAN":
+        vgan = VGAN()
+        vgan.load_models(Path() / "experiments" / "VGAN" /
+                         f"VGAN_{dataset_name}" / "models" / "generator_0.pt", ndims=X_train.shape[1])
+    elif gen_model_to_use == "VMMD":
+        vgan = VMMD()
+        vgan.load_models(Path() / "experiments" / "VMMD" /
+                         f"VMMD_{dataset_name}" / "models" / "generator_0.pt", ndims=X_train.shape[1])
+    vgan.approx_subspace_dist()
+
+    return vgan.check_if_myopic(X_train, bandwidth=[
+        1, 0.1, 0.001, 0.0001], count=min(1000, X_train.shape[0]))["recommended bandwidth"].item(), vgan.subspaces.shape[0]
 
 
 if __name__ == "__main__":
-    dataset_name = "MVTec-AD_bottle"
+    logging.basicConfig(level=logging.DEBUG)
+    dataset_name = "Ionosphere"
 
-    auc_vgan_ens = launch_outlier_detection_experiments(dataset_name, [
-        LOF()])
+    auc_vgan_ens = pretrained_launch_outlier_detection_experiments(dataset_name, [
+        LOF()], gen_model_to_use="VMMD")  # ,   epochs=3000, temperature=1)
     print(
         f'AUC obtained by the VGAN-based ensemble model: {print(auc_vgan_ens)}')
-
-    model = LOF()
-    X_train, X_test, y_test = load_data(dataset_name)
-    model.fit(X_train)
-    decision_function_scores = model.decision_function(X_test)
-    auc_model = auc(y_test, decision_function_scores)
-    print(f'AUC obtained by unsembled model: {auc_model}')
