e145e277737d61a9cf22531e3b0a19887cfb6130
jcribeiro98 <jose@cribeiro.de>
2024-09-04 16:46:51 +0200

    Added all the changes made in the Outlier Detection Experiments to the base modules

--- src/models/Generator.py
+++ src/models/Generator.py
@@ -1,22 +1,29 @@
-import torch 
-from torch import nn 
+import torch
+from torch import nn
 
-class upper_softmax(nn.Module):   #Regular function definition does not appear to work properly within a Sequential definition of a network in Pytorchs
+
+# Regular function definition does not appear to work properly within a Sequential definition of a network in Pytorchs
+class upper_softmax(nn.Module):
     def __init__(self):
-       super().__init__() #Dummy intialization as there is no parameter to learn 
-    def forward(self,x):
-        x = torch.nn.functional.softmax(x,1)
-        x = torch.less(x,1/x.shape[1])*x + torch.greater_equal(x,1/x.shape[1])
+        super().__init__()  # Dummy intialization as there is no parameter to learn
+
+    def forward(self, x):
+        x = torch.nn.functional.softmax(x, 1)
+        x = torch.less(x, 1/x.shape[1])*x + \
+            torch.greater_equal(x, 1/x.shape[1])
         return x
+
 
 class upper_lower_softmax(nn.Module):
     def __init__(self):
-       super().__init__() #Dummy intialization as there is no parameter to learn 
-    def forward(self,x):
-        x = torch.nn.functional.softmax(x,1)
-        selected = torch.greater_equal(x,1/x.shape[1])
+        super().__init__()  # Dummy intialization as there is no parameter to learn
+
+    def forward(self, x):
+        x = torch.nn.functional.softmax(x, 1)
+        selected = torch.greater_equal(x, 1/x.shape[1])
         x = x*selected + (~selected)*1e-08
         return x
+
 
 class Generator(nn.Module):
     def __init__(self, latent_size):
@@ -28,9 +35,10 @@ class Generator(nn.Module):
             nn.Linear(latent_size, latent_size),
             upper_softmax()
         )
-    
+
     def forward(self, input):
         return self.main(input)
+
 
 class Generator_big(nn.Module):
     def __init__(self, latent_size, img_size):
@@ -42,6 +50,6 @@ class Generator_big(nn.Module):
             nn.Linear(8*latent_size, img_size),
             upper_softmax()
         )
-    
+
     def forward(self, input):
         return self.main(input)
